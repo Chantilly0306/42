2a9bf7c94c88b61cca6b9c422722eb39ae16b3a1
“Hsiang <miffy7215@gmail.com>
2026-02-22 21:33:26 +0100

    put header

--- C13/ex9/ft_list.h
+++ C13/ex9/ft_list.h
@@ -9,6 +9,16 @@ typedef struct s_list
     void            *data;
 }   t_list;
 
-t_list  *ft_create_elem(void *data);
+t_list  *ft_create_elem(void *data)
+{
+    t_list  *node;
+
+    node = malloc(sizeof(t_list));
+    if (!node)
+        return (0);
+    node->next = NULL;
+    node->data = data;
+    return (node);
+}
 
 #endif
