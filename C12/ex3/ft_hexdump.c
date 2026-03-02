53c56860b0ee71e00d1a287174bed78fdf80d559
“Hsiang <miffy7215@gmail.com>
2026-03-03 00:08:58 +0800

    finish hexdump

--- C12/ex3/ft_hexdump.c
+++ C12/ex3/ft_hexdump.c
@@ -1,94 +1,72 @@
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <libgen.h>
 
-int display_error(char *prog_name, char *file_name)
-{
-    int     i;
-    char    *prog;
-    char    *error_msg;
-
-    prog = basename(prog_name);
-    error_msg = strerror(errno);
-    i = 0;
-    while (prog[i])
-        write(2, &prog[i++], 1);
-    write(2, ": ", 2);
-    i = 0;
-    while (file_name[i])
-        write(2, &file_name[i++], 1);
-    write(2, ": ", 2);
-    i = 0;
-    while (error_msg[i])
-        write(2, &error_msg[i++], 1);
-    write(2, "\n", 1);
-    return (0);
-}
-
-char    *print_size(char *size, int n)
-{
-    int     i;
-    int     pos;
-    char    *hex;
-
-    hex = "0123456789abcdef";
-    size[7] = hex[n];
-    i = 0;
-    pos = 6;
-    while (--pos >= 0)
-    {
-        while (i < 16)
-        {
-            if (size[pos] == hex[i] && size[pos] != 'f')
-            {
-                size[pos] = hex[i + 1];
-                return (write(1, size, 8), size);
-            }
-            else if (size[pos] == 'f')
-            {
-                size[pos] = '0';
-                break ;
-            }
-            i++;
-        }
-    }
-}
-
-void    print_hex(char *buffer)
-{
-    int     i;
-    char    c;
-    char    *hex;
-
-    hex = "0123456789abcdef";
-    i = 0;
-    write(1, "  ", 2);
-    while (buffer[i])
-    {
-        c = hex[buffer[i] / 16];
-        write(1, &c, 1);
-        c = hex[buffer[i] % 16];
-        write(1, &c, 1);
-        if (i == 7 || i == 15)
-            write(1, " ", 1);
-        write(1, " ", 1);
-        i++;
-    }
-}
-
-void    print_c(char *buffer)
+int compare(unsigned char *s1, unsigned char *s2)
 {
     int i;
 
     i = 0;
-    write(1, "|", 1);
-    while (buffer[i])
+    while (i < 16)
     {
-        if (buffer[i] >= 32 && buffer[i] <= 126)
-            write(1, &buffer[i], 1);
+        if (s1[i] != s2[i])
+            return (s1[i] - s2[i]);
+        i++;
+    }
+    return (0);
+}
+
+void    display_error(char *prog_name, char *file_name)
+{
+    int     i;
+    char    *prog;
+    char    *err_msg;
+
+    prog = basename(prog_name);
+    err_msg = strerror(errno);
+    i = 0;
+    while (prog[i])
+        write(1, &prog[i++], 1);
+    write(1, ": ", 2);
+    i = 0;
+    while (file_name[i])
+        write(1, &file_name[i++], 1);
+    write(1, ": ", 2);
+    i = 0;
+    while (err_msg[i])
+        write(1, &err_msg[i++], 1);
+    write(1, "\n", 1);
+}
+
+void    print_addr(long offset)
+{
+    int     i;
+    char    str[8];
+    char    *hex;
+
+    hex = "0123456789abcdef";
+    i = 7;
+    while (i >= 0)
+    {
+        str[i] = hex[offset % 16];
+        offset /= 16;
+        i--;
+    }
+    write(1, str, 8);
+}
+
+void    print_c(unsigned char *buf, int len)
+{
+    int i;
+
+    write(1, "|", 1);
+    i = 0;
+    while (i < len)
+    {
+        if (buf[i] >= 32 && buf[i] <= 126)
+            write(1, &buf[i], 1);
         else
             write(1, ".", 1);
         i++;
@@ -96,107 +74,99 @@ void    print_c(char *buffer)
     write(1, "|\n", 2);
 }
 
-char    *concatenate(char *dest, char *size, char *buffer)
+void    print_line(int len, long offset, unsigned char *buf)
 {
     int     i;
-    int     j;
     char    *hex;
 
     hex = "0123456789abcdef";
+    print_addr(offset);
+    write(1, "  ", 2);
     i = 0;
-    j = 0;
-    while (size[j])
-        dest[i++] = size[j++];
-    dest[i++] = ' ';
-    dest[i++] = ' ';
-    j = 0;
-    while (buffer[j])
+    while (i < 16)
     {
-        dest[i] = hex[buffer[j] / 16];
-        dest[i + 1] = hex[buffer[j] / 16];
-        i += 2;
-        if (j == 7 || j == 15)
-            dest[i++] = ' ';
-        dest[i++] = ' ';
-    }
-    while (i < 78)
-        dest[i++] = '\0';
-    return (dest);
-}
-
-void    between_files(int fd, char *last_line, char *buffer)
-{
-    int     i;
-    char    size[8];
-
-    i = 0;
-    while (i < 8)
-    {
-        size[i] = last_line[i];
+        if (i < len)
+        {
+            write(1, &hex[buf[i] / 16], 1);
+            write(1, &hex[buf[i] % 16], 1);
+        }
+        else
+            write(1, "  ", 2);
+        write(1, " ", 1);
+        if (i == 7 || i == 15)
+            write(1, " ", 1);
         i++;
     }
-    i = 0;
-    while (last_line[i])
-        write(1, &last_line[i++], 1);
-    
+    print_c(buf, len);
 }
 
-char    *read_and_hex(int fd, int first_line, char *last_line, char buffer[16])
+void    handle_file(int fd, int *len, long *offset, unsigned char *buf)
 {
-    int     ret;
-    char    *size;
-    char    buffer[16];
+    int             repeat;
+    int             i;
+    unsigned char   c;
+    unsigned char   tmp[16];
 
-    size = "00000000";
-    ret = read(fd, buffer, 16);
-    while (ret == 16)
+    while (read(fd, &c, 1) > 0)
     {
-        if (first_line)
-            write(1, size, 8);
-        else
-            size = print_size(size, 0);
-        first_line = 0;
-        print_hex(buffer);
-        print_c(buffer);
-        ret = read(fd, buffer, 16);
+        buf[(*len)++] = c;
+        if (*len == 16)
+        {
+            if (compare(tmp, buf))
+            {
+                print_line(16, *offset, buf);
+                repeat = 0;
+            }
+            else if (repeat++ == 0)
+                write(1, "*\n", 2);
+            *offset += *len;
+            *len = 0;
+            i = -1;
+            while (++i < 16)
+                tmp[i] = buf[i];
+        }
     }
-    while (ret < 16)
-    {
-        buffer[ret] == '\0';
-        ret++;
-    }
-    last_line = concatenate(last_line, size, buffer);
-    return (last_line);
+    close(fd);
 }
 
-// 78 bytes a line
+void    print_endline(int len, long offset, unsigned char *buf)
+{
+    if (len > 0)
+    {
+        print_line(len, offset, buf);
+        offset += len;
+    }
+    if (offset > 0)
+    {
+        print_addr(offset);
+        write(1, "\n", 1);
+    }
+}
 
 int main(int argc, char **argv)
 {
-    int     i;
-    int     fd;
-    int     first_line;
-    char    *last_line;
-    char    buffer[16];
+    int             i;
+    int             fd;
+    int             len;
+    long            offset;
+    unsigned char   buf[16];
 
+    i = 1;
+    len = 0;
+    offset = 0;
     if (argc == 2)
-        read_and_hex(0);
+        handle_file(0, &len, &offset, buf);
     else
     {
-        i = 2;
-        first_line = 1;
-        while (i < argc)
+        while (++i < argc)
         {
             fd = open(argv[i], O_RDONLY);
             if (fd == -1)
                 display_error(argv[0], argv[i]);
             else
-            {
-                last_line = read_and_hex(fd, first_line, last_line, buffer);
-                first_line = 0;
-            }
+                handle_file(fd, &len, &offset, buf);
         }
-        print_size(size, n);
+        print_endline(len, offset, buf);
     }
     return (0);
 }
