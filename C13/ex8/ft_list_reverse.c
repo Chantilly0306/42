5c9056d1a30fc63aeeef9f84908704228d449023
“Hsiang <miffy7215@gmail.com>
2026-03-04 09:57:07 +0800

    simplify list reverse

--- C13/ex8/ft_list_reverse.c
+++ C13/ex8/ft_list_reverse.c
@@ -1,20 +1,45 @@
 #include "ft_list.h"
+#include <stdio.h>
 
 void    ft_list_reverse(t_list **begin_list)
 {
-    t_list  *current = begin_list;
-    t_list  *prev = NULL;
-    t_list  *next;
+    t_list  *current;
+    t_list  *next_node;
 
     if (!begin_list || !*begin_list)
         return ;
-    current = *begin_list;
-    while (current)
+    next_node = NULL;
+    while (*begin_list)
     {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
+        current = *begin_list;
+        *begin_list = (*begin_list)->next;
+        current->next = next_node;
+        next_node = current;
     }
-    *begin_list = prev;
+    *begin_list = current;
 }
+
+/*int main(void)
+{
+    int     i = 0;
+    char    *strs[] = {"Apple", "Banana", "Candy"};
+    t_list  *tmp = NULL;
+    t_list  *new_list;
+
+    while (i < 3)
+    {
+        new_list = ft_create_elem(strs[i]);
+        new_list->next = tmp;
+        tmp = new_list;
+        i++;
+    }
+    printf("%p\n", new_list);
+    printf("%p\n", new_list->next);
+    printf("%p\n", new_list->next->next);
+    printf("%s\n\n", (char *)(new_list->data));
+    ft_list_reverse(&new_list);
+    printf("%p\n", new_list);
+    printf("%p\n", new_list->next);
+    printf("%p\n", new_list->next->next);
+    printf("%s\n\n", (char *)(new_list->data));
+}*/
