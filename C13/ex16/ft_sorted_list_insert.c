9ffae22d55f3c7081e210d82f81f3b80dd50f307
“Hsiang <miffy7215@gmail.com>
2026-03-21 11:57:20 +0800

    sorted list insert

--- C13/ex16/ft_sorted_list_insert.c
+++ C13/ex16/ft_sorted_list_insert.c
@@ -4,7 +4,40 @@
 void    ft_sorted_list_insert(t_list **begin_list, void *data,
         int (*cmp)(void *, void *))
 {
+    int     swapped;
+    t_list  *dummy;
+    t_list  *prev;
+    t_list  *curr;
+    t_list  *next;
 
+    if (!begin_list || !*begin_list || !cmp)
+        return ;
+    dummy = ft_create_elem(NULL);
+    curr = ft_create_elem(data);
+    dummy->next = curr;
+    curr->next = *begin_list;
+    swapped = 1;
+    while (swapped)
+    {
+        swapped = 0;
+        prev = dummy;
+        curr = prev->next;
+        while (curr->next)
+        {
+            next = curr->next;
+            if (cmp(curr->data, next->data) > 0)
+            {
+                curr->next = next->next;
+                prev->next = next;
+                next->next = curr;
+                swapped = 1;
+            }
+            else
+                curr = next;
+            prev = prev->next;
+        }
+    }
+    *begin_list = dummy->next;
 }
 
 int cmp(void *data1, void *data2)
